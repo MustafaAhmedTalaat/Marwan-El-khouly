803133aca9e8719cf3532892fab9e650d0960263
MustafaAhmedTalaat <mustafaahmedtlt13579@gmail.com>
2025-09-27 16:53:46 +0300

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Footer.css";
+
+const links = [
+  { title: "الرئيسية", href: "main" },
+  { title: "من انا", href: "#me" },
+  { title: "اعمالي", href: "#works" },
+  { title: "المطورين", href: "#devs" },
+  { title: "تواصل معي", href: "#contact" }
+];
+
+const Footer = () => (
+  <footer className="footer">
+    <nav className="footer-nav">
+      {links.map(link => (
+        <a
+        className="links"
+          key={link.href}
+          href={link.href}
+        >
+          {link.title}
+        </a>
+      ))}
+    </nav>
+    <div className="footer-copy">
+      <span>جميع الحقوق محفوظة © مروان الخولي 2025</span>
+      <span>تم تطوير هذا الموقع المطورين - م/مصطفى جنيدي - م/مصطفى طلعت </span>
+    </div>
+  </footer>
+);
+
+export default Footer;
